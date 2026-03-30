54f45e83de9366de7365707837a9b1bb357e5a2a
Ankita Ray <rayankita181000@gmail.com>
2026-03-30 12:14:24 +0530

    router fixed

--- backend/routes/enquiryRoutes.js
+++ backend/routes/enquiryRoutes.js
@@ -1,11 +1,18 @@
+const express = require("express");
+const router = express.Router();
 const {
   createEnquiry,
   getAllEnquiries,
   updateEnquiryStatus, 
 } = require("../controllers/enquiryController");
-
+console.log("Check createEnquiry:", typeof createEnquiry);
+console.log("Check getAllEnquiries:", typeof getAllEnquiries);
+console.log("Check updateEnquiryStatus:", typeof updateEnquiryStatus);
 router.post("/", createEnquiry);
 router.get("/", getAllEnquiries);
 
 // ✅ NEW ROUTE
-router.patch("/:id/status", updateEnquiryStatus);
+router.patch("/:id/status", (req, res, next) => {
+    updateEnquiryStatus(req, res, next);
+});
+module.exports = router;
