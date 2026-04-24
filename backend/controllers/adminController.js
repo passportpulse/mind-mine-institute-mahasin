701a491375d3f1cdc9211d5d981a674f522a0464
passportpulse <mypassportpulse@gmail.com>
2026-04-24 17:07:29 +0530

    Update adminController.js

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,7 +2,7 @@ exports.adminLogin = async (req, res) => {
   const { id, password } = req.body;
 
   // 🔐 Fixed credentials (for now)
-  if (id === "admin" && password === "1234") {
+  if (id === "admin" && password === "Mind@2026") {
     return res.json({
       success: true,
       message: "Login successful",
